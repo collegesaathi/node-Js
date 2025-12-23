f0a47e5b7714ea6f5ad9a89330e160e604ecd562
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-23 10:26:15 +0530

    manages ad

--- src/routes/CommonRoute.js
+++ src/routes/CommonRoute.js
@@ -2,11 +2,8 @@ const express = require("express");
 const { List ,University } = require("../Controllers/CommonController");
 const router = express.Router();
 
-
 router.get("/all/catergoy/university", List);
 
 router.get("/all/university", University);
-
-
 
 module.exports = router;
