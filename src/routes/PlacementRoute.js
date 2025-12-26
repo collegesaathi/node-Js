dc15751089791495e7351e7a16820d95e3f751e6
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-26 10:35:14 +0530

    specialisationRoute2

--- src/routes/PlacementRoute.js
+++ src/routes/PlacementRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 // const dynamicUpload = require("../utils/dynamicUpload");
-const {  ApprovalSoftDelete,  PlacementSoftDelete, ApprovalandPlacements } = require("../Controllers/PlacementController");
+// const {  ApprovalSoftDelete,  PlacementSoftDelete, ApprovalandPlacements } = require("../Controllers/PlacementController");
 // router.post("/approval/add", dynamicUpload("approvals").single("image"), ApprovalAdd);
 // router.post("/approval/edit", dynamicUpload("approvals").single("image"), ApprovalEdit);
-router.get("/approval/delete/:id", ApprovalSoftDelete)
-// router.post("/placement/add",  dynamicUpload("placements").single("image"), PlacementAdd);
-//  router.post("/placement/edit",  dynamicUpload("placements").single("image"), PlacementEdit);
-router.get("/placement/delete/:id", PlacementSoftDelete)
-router.get("/placement/approval", ApprovalandPlacements)
+// router.get("/approval/delete/:id", ApprovalSoftDelete)
+// // router.post("/placement/add",  dynamicUpload("placements").single("image"), PlacementAdd);
+// //  router.post("/placement/edit",  dynamicUpload("placements").single("image"), PlacementEdit);
+// router.get("/placement/delete/:id", PlacementSoftDelete)
+// router.get("/placement/approval", ApprovalandPlacements)
 module.exports = router;
--- src/routes/universityRoutes.js
+++ src/routes/universityRoutes.js
@@ -4,19 +4,12 @@ const { allUniversities, adminapprovalsplacements, adminUniversitylisting, addUn
 const dynamicUpload = require("../utils/dynamicUpload");
 
 router.get("/universities", allUniversities);
-
 router.get("/admin/approvalandpartners", adminapprovalsplacements);
-
 router.get("/admin/universities/listing", adminUniversitylisting);
-
 router.post("/admin/universities/add", dynamicUpload("universities").any(), addUniversity);
-
 router.get("/admin/university", allAdminUniversities);
-
 router.get("/university/delete/:id", universitiesDelete)
-
 router.get("/university/:slug", getUniversityById)
-
 router.post("/admin/universities/update", dynamicUpload("universities").any(), updateUniversity)
 
 module.exports = router;
