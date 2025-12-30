36f070f1c37c7fbd5776711d6cc99e9d1160e7fc
collegesaathi <nemrajcollegesathi.com@gmail.com>
2025-12-30 16:12:35 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -20,8 +20,8 @@ const path = require("path");
 app.use(
   cors({
     origin: [
-      "https://amityonlines.com",
-      "https://www.amityonlines.com"
+      "https://indiaprograms.com",
+      "https://www.indiaprograms.com"
     ],
     credentials: true,
     methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
