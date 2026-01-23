e7cc3d8dd4be836360f07d4902559f80a7d2a5c6
collegesaathi <nemrajcollegesathi.com@gmail.com>
2026-01-23 10:46:04 +0530

    Compare Now

--- src/routes/clickpickRoutes.js
+++ src/routes/clickpickRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { ClickPickDelete, updateRecord, GetClickpickData, AddClickPick, GetClickPickListData, compareData, compareSpeData } = require("../controllers/ClickPickController");
 const router = express.Router();
 router.post("/admin/clickpick/add", AddClickPick);
-router.post('/admin/clickpick/update', updateRecord)
-router.delete("/clickpick/delete/:id", ClickPickDelete)
+router.post('/admin/clickpick/update', updateRecord);
+router.delete("/clickpick/delete/:id", ClickPickDelete);
 //View Click Pick 
 router.get('/clickpick', GetClickpickData)
 //Card Click PIck
